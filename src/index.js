1401bc7b3630ddec317491d1c975a655b6f1dfb1
nanasi-1 <test-email>
2025-03-16 15:25:00 +0900

    ルール追加

--- src/index.js
+++ src/index.js
@@ -11,20 +11,40 @@ console.debug("[Remove Numbers for Zenn] running...")
 const defaultRules = [
   {
     regex: RegExp("/.+/articles/.+"),
-    selectors: ["LikeButton_likedCount__", "BookmarkButton_bookmarkedCount__"],
+    selectors: ["LikeButton_likedCount__", "BookmarkButton_bookmarkedCount__", "ArticleList_like__"],
+  },
+  {
+    regex: RegExp("/.+/books/.+"),
+    selectors: ["LikeButton_likedCount__"],
+  },
+  {
+    regex: RegExp("/.+/scraps/.+"),
+    selectors: ["LikeButton_likedCount__", "View_commentsCount__"],
   },
   {
     regex: RegExp("/$"),
     selectors: ["ArticleList_like__", "ArticleList_date__", "BookLink_likedCount__"],
   },
   {
-    regex: RegExp("/articles/explore$"),
+    regex: RegExp("/articles"),
     selectors: ["ArticleList_like__", "ArticleList_date__", "BookLink_likedCount__"]
+  },
+  {
+    regex: RegExp("/books"),
+    selectors: ["BookLink_likedCount__", "BookLargeLink_likedCount__"]
+  },
+  {
+    regex: RegExp("/topics/.+"),
+    selectors: ["ArticleList_like__", "BookLink_likedCount__"]
   },
   {
     regex: RegExp("/search"),
     selectors: ["ArticleList_like__", "BookLink_likedCount__"]
   },
+  {
+    regex: RegExp("/[^/]+"), // プロフィール
+    selectors: ["UserHeader_count__", "ArticleCard_likes__", "UserHeader_tabItemCount__I_", "ScrapRow_commentsCount__"]
+  }
 ]
 
 window.addEventListener('changeURL', () => {
