56d09ebb56936c3a559fef925e2171d476e3159e
nanasi-1 <test-email>
2025-03-16 14:12:14 +0900

    最初に開いたページで削除確認

--- src/features.js
+++ src/features.js
@@ -17,4 +17,12 @@ export function isPathMatching(regex) {
   return regex.test(window.location.pathname)
 }
 
-// * @param {{ selectors: string[], regex: string }} rule 
+/**
+ * ルールに基づいて現在のページの要素を削除する
+ * @param {{ selectors: string[], regex: string }[]} rules
+ */
+export function removeWithRules(rules) {
+  for (const rule of rules) {
+    if (isPathMatching(rule.regex)) removeElems(rule.selectors)
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-import { removeElems, isPathMatching } from "./utils.js";
+import { removeWithRules } from "./features.js";
 
 console.log("[Remove Numbers for Zenn] running...")
+
+/**
+ * パスに対してどのクラスを削除するかのルール
+ * いつかカスタマイズできるようにしたい
+ * @type {{ selectors: string[], regex: string }[]}
+ */
+const defaultRules = [
+  {
+    selectors: ["LikeButton_likedCount__", "BookmarkButton_bookmarkedCount__"],
+    regex: RegExp("/.+/articles/.+"),
+  }
+]
+
+removeWithRules(defaultRules)
